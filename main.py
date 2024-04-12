9f30daad909ec0895889c8423589f12660970cba
salty-muffin <t.babel@gmx.com>
2024-04-12 12:26:07 +0200

    added extra options

--- main.py
+++ main.py
@@ -10,12 +10,23 @@ from dotenv import dotenv_values
 from email.mime.text import MIMEText
 
 
+# fmt: off
 @click.command()
-@click.option("--csv", type=click.Path(exists=True, dir_okay=False))
-@click.option("--template", type=click.File("r"))
-@click.option("--subject", type=str)
-@click.option("--bcc", type=str, required=False)
-def send_mails(csv: str, template: TextIO, subject: str, bcc: str | None) -> None:
+@click.option("--csv", type=click.Path(exists=True, dir_okay=False), help="path to the csv containing all people to send the mail to")
+@click.option("--mail_column", type=str, help="the name of the column of the email address of the recipients")
+@click.option("--template", type=click.File("r"), help="path to the text template for the mail \{*column name*\} will be swappend for the appropriate data from the csv")
+@click.option("--subject", type=str, help="subject of the mail")
+@click.option("--bcc", type=str, required=False, help="blind copy for the mail")
+@click.option("--interval", type=int, default=3, help="pause between each sending of a mail in seconds")
+# fmt: on
+def send_mails(
+    csv: str,
+    mail_column: str,
+    template: TextIO,
+    subject: str,
+    bcc: str | None,
+    interval: int,
+) -> None:
     config = dotenv_values(".env")
     table = pd.read_csv(csv)
 
@@ -29,7 +40,7 @@ def send_mails(csv: str, template: TextIO, subject: str, bcc: str | None) -> Non
         print(f"could not establish connection to {config['smtp_server']}")
     else:
         for index, mail in tqdm.tqdm(
-            zip(table.index, table[config["mail_column"]]), total=table.shape[0]
+            zip(table.index, table[mail_column]), total=table.shape[0]
         ):
             text = template.read()
             subbed_text = re.sub(
@@ -53,7 +64,7 @@ def send_mails(csv: str, template: TextIO, subject: str, bcc: str | None) -> Non
                 print(f"could not send message to {mail}")
                 not_sent.append(mail)
 
-            time.sleep(3)
+            time.sleep(interval)
 
     conn.quit()
 
